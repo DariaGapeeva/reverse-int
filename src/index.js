19a7e51b88d857340ea1fc60f2453df548a24252
DariaGapeeva <d.gapeeva@gmail.com>
2021-01-30 19:53:25 +0700

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let array = String(n).split("").reverse();
+
+    if (array[array.length - 1] === "-") {
+        array.pop();
+    }
+    return Number(array.join(""));
+};
